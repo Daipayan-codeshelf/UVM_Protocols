638bba4837ad90a947dd757a0edfc062e89116e2
Daipayan <95981461+Daipayan-codeshelf@users.noreply.github.com>
2026-06-01 17:20:58 +0530

    Add apb_seq_item class for APB sequence items

---
+++ USART/TestBench/apb_seq_item.sv
@@ -0,0 +1,17 @@
+class apb_seq_item extends uvm_sequence_item;
+
+   rand bit        pwrite;
+   rand bit [7:0]  paddr;
+   rand bit [31:0] pwdata;
+
+   `uvm_object_utils_begin(apb_seq_item)
+      `uvm_field_int(pwrite,UVM_ALL_ON)
+      `uvm_field_int(paddr ,UVM_ALL_ON)
+      `uvm_field_int(pwdata,UVM_ALL_ON)
+   `uvm_object_utils_end
+
+   function new(string name="apb_seq_item");
+      super.new(name);
+   endfunction
+
+endclass
