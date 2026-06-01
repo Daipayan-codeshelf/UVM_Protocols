25fad786eb7304ea18b1b2201655e92d06abc8ce
Daipayan <95981461+Daipayan-codeshelf@users.noreply.github.com>
2026-06-01 17:22:53 +0530

    Add usart_env class for UVM environment setup

---
+++ USART/TestBench/env.sv
@@ -0,0 +1,33 @@
+class usart_env extends uvm_env;
+
+   `uvm_component_utils(usart_env)
+
+   apb_agent   apb_agt;
+   usart_agent usart_agt;
+
+   function new(string name,
+                uvm_component parent);
+
+      super.new(name,parent);
+
+   endfunction
+
+   function void build_phase(uvm_phase phase);
+
+      super.build_phase(phase);
+
+      apb_agt =
+      apb_agent::type_id::create(
+         "apb_agt",
+         this
+      );
+
+      usart_agt =
+      usart_agent::type_id::create(
+         "usart_agt",
+         this
+      );
+
+   endfunction
+
+endclass
