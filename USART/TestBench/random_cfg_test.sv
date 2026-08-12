30716bf10562ed010a20a12b1438338f9b80cc75
Daipayan <95981461+Daipayan-codeshelf@users.noreply.github.com>
2026-08-13 02:02:09 +0530

    Add random_cfg_test class for UVM testing

---
+++ USART/TestBench/random_cfg_test.sv
@@ -0,0 +1,69 @@
+class random_cfg_test extends uvm_test;
+
+   `uvm_component_utils(random_cfg_test)
+
+   usart_env env;
+   virtual usart_if vif;
+
+   function new(
+      string name="random_cfg_test",
+      uvm_component parent=null
+   );
+      super.new(name,parent);
+   endfunction
+
+   function void build_phase(
+      uvm_phase phase
+   );
+
+      super.build_phase(phase);
+
+      env =
+      usart_env::type_id::create(
+         "env",
+         this
+      );
+
+      if(!uvm_config_db#
+         (virtual usart_if)::get(
+            this,
+            "",
+            "vif",
+            vif
+         ))
+         `uvm_fatal(
+            "NOVIF",
+            "No interface"
+         );
+
+   endfunction
+
+   task run_phase(
+      uvm_phase phase
+   );
+
+      random_cfg_seq seq;
+
+      phase.raise_objection(this);
+
+      wait(vif.presetn);
+
+      repeat(2)
+         @(posedge vif.pclk);
+
+      seq =
+      random_cfg_seq::type_id::create(
+         "seq"
+      );
+
+      seq.start(
+         env.apb_agt.seqr
+      );
+
+      #10000000;
+
+      phase.drop_objection(this);
+
+   endtask
+
+endclass
