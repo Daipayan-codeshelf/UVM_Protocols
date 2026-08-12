da50aeb32bbd733ed7c116b527a80db37ad42f8e
Daipayan <95981461+Daipayan-codeshelf@users.noreply.github.com>
2026-08-13 01:58:29 +0530

    Create data_len_5_test.sv

---
+++ USART/TestBench/data_len_5_test.sv
@@ -0,0 +1,85 @@
+class data_len_5_test extends uvm_test;
+
+   `uvm_component_utils(data_len_5_test)
+
+   usart_env env;
+   virtual usart_if vif;
+
+   function new(
+      string name="data_len_5_test",
+      uvm_component parent=null
+   );
+
+      super.new(name,parent);
+
+   endfunction
+
+   function void build_phase(
+      uvm_phase phase
+   );
+
+      super.build_phase(phase);
+
+      env =
+      usart_env::type_id::create(
+         "env",
+         this
+      );
+
+      if(!uvm_config_db#
+         (virtual usart_if)::get(
+            this,
+            "",
+            "vif",
+            vif
+         ))
+      begin
+         `uvm_fatal(
+            "NOVIF",
+            "No interface"
+         );
+      end
+
+   endfunction
+
+   task run_phase(
+      uvm_phase phase
+   );
+
+      data_len_5_seq seq;
+
+      phase.raise_objection(this);
+
+      //--------------------------------
+      // Wait for reset release
+      //--------------------------------
+
+      wait(vif.presetn == 1);
+
+      repeat(2)
+         @(posedge vif.pclk);
+
+      //--------------------------------
+      // Start sequence
+      //--------------------------------
+
+      seq =
+      data_len_5_seq::type_id::create(
+         "seq"
+      );
+
+      seq.start(
+         env.apb_agt.seqr
+      );
+
+      //--------------------------------
+      // Allow RX completion
+      //--------------------------------
+
+      #20000000;
+
+      phase.drop_objection(this);
+
+   endtask
+
+endclass
